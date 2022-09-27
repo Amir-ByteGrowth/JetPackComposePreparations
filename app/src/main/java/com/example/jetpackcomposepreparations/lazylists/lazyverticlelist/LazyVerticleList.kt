dd4f4e16925927d12c5bcf83da521c826e817ad7
Amir javeed <ranaamir347@gmail.com>
2022-09-27 15:51:18 +0400

    add lazylist state and state.animate to specific position

--- app/src/main/java/com/example/jetpackcomposepreparations/lazylists/lazyverticlelist/LazyVerticleList.kt
+++ app/src/main/java/com/example/jetpackcomposepreparations/lazylists/lazyverticlelist/LazyVerticleList.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -15,29 +18,46 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposepreparations.R
 import com.example.jetpackcomposepreparations.sample.SampleData
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun CreateLazyVerticalList() {
-    LazyColumn(
+
+    var state = rememberLazyListState()
+    var coroutineScope = rememberCoroutineScope()
+
+    Column(
         modifier = Modifier
-            .padding(10.dp)
             .fillMaxWidth()
-            .fillMaxHeight(), verticalArrangement = Arrangement.Center
+            .padding(vertical = 20.dp), horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        items(SampleData.nameList) { item ->
-            Box(modifier = Modifier.height(70.dp)) {
-                Text(
-                    text = item,
-                    modifier = Modifier.padding(top = 20.dp, start = 10.dp)
+        Button(onClick = { coroutineScope.launch { state.animateScrollToItem(0) } }) {
+            Text(text = "ClickHere")
+        }
+        LazyColumn(
+            state = state,
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(), verticalArrangement = Arrangement.Center
+        ) {
+            items(SampleData.nameList) { item ->
+                Box(modifier = Modifier.height(70.dp)) {
+                    Text(
+                        text = item,
+                        modifier = Modifier.padding(top = 20.dp, start = 10.dp)
+                    )
+                }
+                Divider(
+                    color = colorResource(id = R.color.black)
                 )
             }
-            Divider(
-                color = colorResource(id = R.color.black)
-            )
         }
     }
+
 }
+
 
 @Composable
 fun CreateLazyVerticalListWithIndex() {
